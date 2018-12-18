3670bf4670fd099ddfc55e075ccef4db9defa1a9
Joel Kim <kim.dohhyoung@gmail.com>
2018-12-18 15:41:32 +0900

    add create_ghpages.sh

---
+++ create_ghpages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+git add . -A
+git commit -m "automatic commit"
+git push origin korean
+
+git checkout -B gh-pages
+git rebase korean
+
+touch .nojekyll
+
+echo '!build/' >> .gitignore
+echo '!env/' >> .gitignore
+
+cd doc
+make -e SPHINXOPTS="-D language='ko'" html
+cd ..
+
+git add . -A
+git commit -m "build"
+git push -f origin gh-pages
+git reset --hard HEAD
+git clean -f
+git checkout korean
