a1f8ff64669421edfafc5e33c6ea0b6d4f9859d4
Joel Kim <kim.dohhyoung@gmail.com>
2018-12-19 14:23:00 +0900

    automatic commit

--- create_ghpages.sh
+++ create_ghpages.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-python -m venv env
-env/bin/python -m pip install -r requirements.txt
-
 git add . -A
 git commit -m "automatic commit"
 git push origin korean
@@ -16,7 +13,7 @@ echo '!_build/' >> .gitignore
 echo '!env/' >> .gitignore
 
 cd doc
-../env/bin/make -e SPHINXOPTS="-D language='ko'" html
+make -e SPHINXOPTS="-D language='ko'" html
 cd ..
 
 git add . -A
